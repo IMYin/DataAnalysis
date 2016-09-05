4f8d64295f7de3aca4f0e3de604e32b752c68465
sunnyin <sunnyin127@gmail.com>
2016-09-05 16:56:48 +0800

    Create Bayes.py

---
+++ Bayes.py
@@ -0,0 +1,13 @@
+#-*-coding=utf-8-*-
+from numpy import *
+
+def trainNB0(trainMatrix,trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory)/float(numTrainDocs)
+    p0Num = zeros(numWords)
+    p1Num = zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+        
