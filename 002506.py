787ec1944bfbcaeb9ff86519373053e59c2c1b06
sunnyin <sunnyin127@gmail.com>
2016-10-12 17:32:09 +0800

    Update 002506.py

--- 002506.py
+++ 002506.py
@@ -39,33 +39,33 @@ def get_macd(sorted_data):
     operator = ''
     if macd[-1] > 0 and macdsignal[-1] > 0:
         if macd[-1] > macdsignal[-1] and macd[-2] <= macdsignal[-2]:
-                operator += 'B!'  #买入
+                operator += 'BX'  #买入
     elif macd[-1] < 0 and macdsignal[-1] < 0:
         if macd[-1] <= macdsignal[-2]:
-                operator += 'S.'
+                operator += 'SX'
     #DEA线与k线发生背离，行情反转信号
     if ma5[-1] >= ma10[-1] and ma10[-1] >= ma20[-1]:  #k线上升
         if SignalMA5[-1] <= SignalMA10[-1] and SignalMA10[-1] <= SignalMA20[-1]:  #DEA下降
-            operator += 'S.'
+            operator += 'S><'
     if ma5[-1] <= ma10[-1] and ma10[-1] <= ma20[-1]:  #k线下降
         if SignalMA5[-1] >= SignalMA10[-1] and SignalMA10[-1] >= SignalMA20[-1]:  #DEA上升
-            operator += 'B.'
+            operator += 'B><'
     if macd[-1] > 0 and macdhist[-1] >0:
         if macd[-1] > macd[-2] and macdhist[-1] > macdhist[-2]:
-            operator += 'B!'
+            operator += 'B^'
     elif macd[-1] < 0 and macdhist[-1] < 0:
         if macd[-1] < macd[-2] and macdhist[-1] > macdhist[-2]:
-            operator += 'S!'
+            operator += 'S^'
     #分析MACD柱状图，由负变正，则买入信号
     if macdhist[-1] > 0:
         for i in range(1,7):
             if macdhist[-2] <= 0:
-                operator += 'B$'
+                operator += 'Bh'
                 break
     if macdhist[-1] < 0:
         for i in range(1,7):
             if macdhist[-2] >= 0:
-                operator += 'S$'
+                operator += 'Sh'
                 break
     return (operator)
     
@@ -83,27 +83,27 @@ def get_kdj(sorted_data):
     
     #1.K线是快速确认线——数值在90以上为超买，数值在10以下为超卖；D大于80时，行情呈现超买现象。D小于20时，行情呈现超卖现象。
     if slowk[-1] >= 90:
-        operator += 'S@'
+        operator += 'S9'
     elif slowk[-1] <= 10:
-        operator += 'B@'
+        operator += 'B1'
     elif slowd[-1] >=80:
-        operator += 'S@'
+        operator += 'S8'
     elif slowd[-1] <= 20:
-        operator += 'B@'
+        operator += 'B2'
 
      #2.上涨趋势中，K值大于D值，K线向上突破D线时，为买进信号
     if slowk[-1] > slowd[-1] and slowk[-2] <= slowd[-2]:
-        operator += 'B#'
+        operator += 'BX'
     elif slowk[-1] < slowd[-1] and slowk[-2] >= slowd[-2]:
-        operator += 'S#'
+        operator += 'SX'
         
      #3.当随机指标与股价出现背离时，一般为转势的信号。
     if ma5[-1] >= ma10[-1] and ma10[-1] >= ma20[-1]:  #k线上升
         if (slowkMA5[-1] <= slowkMA10[-1] and slowkMA10[-1] <= slowkMA20[-1]) or (slowdMA5[-1] <= slowdMA10[-1] and slowdMA10[-1] <= slowdMA20[-1]):
-            operator += 'S.'
+            operator += 'S><'
     elif ma5[-1] <= ma10[-1] and ma10[-1] <= ma20[-1]:  #k线下降
         if (slowkMA5[-1] >= slowkMA10[-1] and slowkMA10[-1] >= slowkMA20[-1]) or (slowdMA5[-1] >= slowdMA10[-1] and slowdMA10[-1] >= slowdMA20[-1]):
-            operator += 'B.'
+            operator += 'B><'
     return operator
 
 def get_bbands(sorted_data):
@@ -114,10 +114,10 @@ def get_bbands(sorted_data):
 #    score = 0
     index_b = (close[-1]-lowerband[-1]) / (upperband[-1]-lowerband[-1])
     if index_b > 1:
-        operator += 'S!'
+        operator += 'S%'
 #        score -= 10
     elif index_b <= 0 :
-        operator += 'B!'
+        operator += 'B%'
 #        score += 10
 
     #通过开口走向判别买入还是卖出
@@ -125,13 +125,13 @@ def get_bbands(sorted_data):
     down = lowerband[-2] -lowerband[-1]
     if up > 0 and down > 0:
         if up > down:
-            operator += 'B#'
+            operator += 'BO'
         elif up < down:
-            operator += 'S#'
+            operator += 'SO'
         
     if ma5[-1] > ma10[-1] and ma10[-1] > ma20[-1]:
         if upperband[-1] < upperband[-2]:
-            operator += 'S!'
+            operator += 'S.'
     return operator
 
 
