3d5d35d23ccc40a292a1fd0db18921bf0e9b8c9a
IMYin <sunnyin127@gmail.com>
2016-11-01 23:03:47 +0800

    update the data path

--- stock/getAllData.py
+++ stock/getAllData.py
@@ -10,7 +10,8 @@ Created on Sat Oct 08 22:59:02 2016
 import tushare as ts
 import os
 
-os.chdir('E:\\big_data\\TheRoadOfPython2016\\anocondaLearningSpace\\data_base')
+path = '/home/sunnyin/myProject/data/stock'
+os.chdir(path)
 x = 0
 stock_basics = ts.get_stock_basics()
 for code in stock_basics.index:
