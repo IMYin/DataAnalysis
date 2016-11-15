76176b891e1fe2ad0997b46eb81781270088f5b7
IMYin <sunnyin127@gmail.com>
2016-11-15 22:47:03 +0800

    update model for the Titanic

--- kaggle/kaggle_titanic.py
+++ kaggle/kaggle_titanic.py
@@ -5,14 +5,14 @@ Created on Thu Nov 10 15:52:01 2016
 @author: IMYin
 """
 
-import os
+import os,datetime
 
-import pandas as pd
 import numpy as np
-
-from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
 import sklearn.preprocessing as preprocessing
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor,RandomForestClassifier
+
+
 #processing the bad value
 def set_missing_ages(df):
     age_df = df[['Age','Fare','Parch','SibSp','Pclass']]
@@ -41,7 +41,8 @@ def set_Cabin_type(df):
     return df
 
 
-os.chdir('E:\\kaggle\\Titanic')    
+t0 = datetime.datetime.now()
+os.chdir('/home/sunnyin/kaggle/data/Titanic')
 data_train = pd.read_csv('train.csv')
 data_train,rfr = set_missing_ages(data_train)
 data_train = set_Cabin_type(data_train)
@@ -56,21 +57,25 @@ df = pd.concat([data_train,dummies_Cabin,dummies_Embarked,dummies_Pclass,dummies
 df.drop(['Pclass','Name','Cabin','Sex','Ticket','Embarked'],axis=1,inplace=True)
 
 scaler = preprocessing.StandardScaler()
-age_scale_param = scaler.fit(df['Age'])
-df['Age_scaled'] = scaler.fit_transform(df['Age'],age_scale_param)
-#fare_scale_param = scaler.fit(df['Fare'])
-#df['Fare_scaled'] = scaler.fit_transform(df['Fare'],fare_scale_param)
+age_scale_param = scaler.fit(df['Age'].reshape(-1,1))
+df['Age_scaled'] = scaler.fit_transform(df['Age'].reshape(-1,1),age_scale_param)
 
-train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+fare_scale_param = scaler.fit(df['Fare'].reshape(-1,1))
+df['Fare_scaled'] = scaler.fit_transform(df['Fare'].reshape(-1,1),fare_scale_param)
+
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Cabin_.*|Fare_.*|Embarked_.*|Sex_.*|Pclass_.*')
 train_np = train_df.as_matrix()
 
 y = train_np[:,0]
 X = train_np[:,1:]
-clf = LogisticRegression()
-clf.fit(X,y)
+rfc = RandomForestClassifier(n_estimators=2000,n_jobs=-1,criterion='entropy')
+rfc.fit(X,y)
+# clf = LogisticRegression()
+# clf.fit(X,y)
 #------------------------------------------------
 
 data_test = pd.read_csv('test.csv')
+data_test.loc[ (data_test.Fare.isnull()), 'Fare' ] = 0
 tmp_df = data_test[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
 null_age = tmp_df[data_test.Age.isnull()].as_matrix()
 X = null_age[:, 1:]
@@ -88,17 +93,20 @@ dummies_Pclass = pd.get_dummies(data_test['Pclass'],prefix='Pclass')
 df_test = pd.concat([data_test,dummies_Cabin,dummies_Embarked,dummies_Pclass,dummies_Sex],axis=1)
 df_test.drop(['Pclass','Name','Cabin','Sex','Ticket','Embarked'],axis=1,inplace=True)
 
-#scaler = preprocessing.StandardScaler()
-#age_scale_param = scaler.fit(df_test['Age'])
-df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'],age_scale_param)
-#fare_scale_param = scaler.fit(df['Fare'])
-#df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'],fare_scale_param)
+df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'].reshape(-1,1),age_scale_param)
+df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'].reshape(-1,1),fare_scale_param)
 
-test = df_test.filter(regex='Age_.*|SibSp|Parch|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+test = df_test.filter(regex='Age_.*|SibSp|Parch|Cabin_.*|Fare_.*|Embarked_.*|Sex_.*|Pclass_.*')
 
 
 #predict the result
-predictions = clf.predict(test)
+predictions = rfc.predict(test)
+# predictions = clf.predict(test)
 
 result = pd.DataFrame({'PassengerId':data_test['PassengerId'], 'Survived':predictions.astype(np.int32)})
-result.to_csv('logistic_regression_predictions.csv',index=False)
+result.to_csv('/home/sunnyin/kaggle/results/rfc_2000.csv',index=False)
+
+t1 = datetime.datetime.now()
+times = (t1-t0).total_seconds()
+
+print("dt:{} sec".format(times))
